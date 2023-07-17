7cde712bf62a296aed0b8876f0eee7efa94708c8
galihagussetyawan <galihdevelopment@gmail.com>
2023-07-17 14:02:01 +0700

    feat: implement a simple REPL

--- main.c
+++ main.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
 
 int main(int argc, char const *argv[])
 {
-    printf("hello world");
+    char input[128];
+
+    while (1)
+    {
+        printf("db > ");
+        fgets(input, (sizeof(input) - 1), stdin);
+
+        if (*input == '\n')
+        {
+            printf("'NULL' is not recognized. \n");
+        }
+
+        input[strlen(input) - 1] = '\0';
+
+        if (*input == '.')
+        {
+            if (strcmp(input, ".exit") == 0)
+            {
+                exit(0);
+            }
+            else
+            {
+                printf("'%s' command is not recognized.\n", input);
+            }
+        }
+    }
+
     return 0;
 }
