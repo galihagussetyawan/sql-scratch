9d50d92ab0caef00dcd686a3aded71c2d6272c4f
galihagussetyawan <galihdevelopment@gmail.com>
2023-07-20 17:52:09 +0700

    feat: harcode init value table

--- main.c
+++ main.c
@@ -3,6 +3,20 @@
 #include <strings.h>
 #include <ctype.h>
 
+typedef struct
+{
+    int id;
+    char name[32];
+    char email[32];
+} row_t;
+
+typedef struct
+{
+    char nama[32];
+    char **head;
+    row_t *rows;
+} table_t;
+
 char *read_input()
 {
     char *str, c = '\0';
@@ -45,17 +59,25 @@ void parse_meta_command(char *input)
     }
 }
 
-void parse_statement(char *input)
+void parse_statement(char *input, table_t *table)
 {
     strlwr(input);
 
-    if (strcmp(input, "insert") == 0)
+    if (strncmp(input, "insert", 6) == 0)
     {
-        printf("inserting ...\n");
+        printf("Inserting ....\n");
+
+        // harcode insert rows
+        table->rows[1].id = 2;
+        strcpy(table->rows[1].name, "agusgalih");
+        strcpy(table->rows[1].email, "agus@gmail.com");
     }
     else if (strcmp(input, "select") == 0)
     {
         printf("selecting ...\n");
+
+        // hardcode select row
+        printf("%d %s %s\n", table->rows[1].id, table->rows[1].name, table->rows[1].email);
     }
     else if (strcmp(input, "delete") == 0)
     {
@@ -67,9 +89,31 @@ void parse_statement(char *input)
     }
 }
 
+table_t *init_table()
+{
+    table_t *table = (table_t *)malloc(sizeof(*table));
+    table->head = malloc(3 * sizeof(table->head));
+    table->head[0] = malloc(32 * sizeof(char));
+    table->head[1] = malloc(32 * sizeof(char));
+    table->head[2] = malloc(32 * sizeof(char));
+    strcpy(table->nama, "users");
+    strcpy(table->head[0], "id");
+    strcpy(table->head[1], "name");
+    strcpy(table->head[2], "email");
+
+    table->rows = (row_t *)malloc(3 * sizeof(*table->rows));
+    table->rows[0].id = 1;
+    strcpy(table->rows[0].name, "galihagus");
+    strcpy(table->rows[0].email, "galih@gmail.com");
+
+    return table;
+}
+
 int main(int argc, char const *argv[])
 {
     char *input;
+    table_t *table = init_table();
+    printf("%d", sizeof(*table->rows) / sizeof(table->rows[0]));
 
     while (1)
     {
@@ -88,7 +132,7 @@ int main(int argc, char const *argv[])
             }
             else
             {
-                parse_statement(input);
+                parse_statement(input, table);
             }
         }
     }
